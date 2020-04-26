b1f21e86111cdb990275d3b05dc14f09e63dd3a0
sv ramu <svramu@gmail.com>
2020-04-26 08:38:05 +0530

    4.4.3 Flex layout. No table. TBD check exam overtime; display and ending.

--- src/app/common/timer.component.ts
+++ src/app/common/timer.component.ts
@@ -4,37 +4,29 @@ import { Lib } from 'app/model/lib';
 @Component({
   selector: 'app-timer',
   template: `
-    <table>
-      <tr>
-        <td>
-          <span id="qsec" class="qsec" [class.hidden]="!showQSec" >
-            {{ timize(qSec) }} - <sp></sp>
-          </span>
-        <td>
-        <td>
-          <span [class.descending]="showTSecToggleReally && isDescending"
-              (click)="toggleDescending()"
-              [class.actionable]="showTSecToggleReally">
-            {{ timize(tSecShow) }}
-          </span>
-          <td>
-        <td>
-          <span class="glyphicon actionable" style="color: blue;"
-            [class.hidden]="!showTSecToggleReally"
-            [class.glyphicon-arrow-up]="!isDescending"
-            [class.glyphicon-arrow-down]="isDescending"
-            (click)="toggleDescending()">
-          </span>
-        <td>
-        <td>
-          <span *ngIf="showMSec" style="color: blue; padding-top: 4px;">
-            <sp></sp><sp></sp><sp></sp><sp></sp>
-            <span>Max. Duration: <span>{{ timize(mSec) }}</span></span>
-          </span>
-        <td>
-      </tr>
-    </table>
-  `,
+    <div fxLayout="row" gdArea="content" style="margin-top: 1px;">
+      <span style="margin-top: 2px;" id="qsec" class="qsec" [class.hidden]="!showQSec" >
+        {{ timize(qSec) }} <sp></sp>
+      </span>
+      <span style="margin-top: 4px; padding: 0px 2px 0px 6px;" [class.descending]="showTSecToggleReally && isDescending"
+          (click)="toggleDescending()"
+          [class.actionable]="showTSecToggleReally">
+        {{ timize(tSecShow) }}
+      </span>
+      <span style="margin-top: 5px;" >
+        <span class="glyphicon actionable" style="color: blue;"
+          [class.hidden]="!showTSecToggleReally"
+          [class.glyphicon-arrow-up]="!isDescending"
+          [class.glyphicon-arrow-down]="isDescending"
+          (click)="toggleDescending()">
+        </span>
+      </span>
+      <span *ngIf="showMSec" style="color: blue; margin-top: 4px;">
+        <sp></sp><sp></sp><sp></sp><sp></sp>
+        <span>Max. Duration: <span>{{ timize(mSec) }}</span></span>
+      </span>
+    </div>
+`,
   styles: [
     '.descending { background-color: yellow; }',
     '.actionable { cursor: pointer; }',
