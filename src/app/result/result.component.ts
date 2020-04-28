24543f4d6848ebbc980de28a939343a20221e1d6
sv ramu <svramu@gmail.com>
2020-04-28 11:33:24 +0530

    4.6.2 Results page code refactored.

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -5,6 +5,23 @@ import { Lib } from '../model/lib';
 import { Marks } from 'app/model/marks';
 import { Bar } from 'app/common/chart.component';
 
+interface ResultObj {
+  isOmitted: boolean,
+  isAttempted: boolean,
+  isGuessing: boolean,
+  isCorrect: boolean,
+  isPartial: boolean,
+
+  duration: number,
+
+  max: number,
+  omitted: number,
+  skipped: number,
+  guess: number,
+  sure: number,
+  scored: number,
+}
+
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
@@ -13,7 +30,7 @@ import { Bar } from 'app/common/chart.component';
 export class ResultComponent implements OnInit {
 
   @Input() result: ExamResult
-  chartArray = []
+  chartArray: Bar[] = []
 
   constructor(private router: Router) { }
 
@@ -21,54 +38,54 @@ export class ResultComponent implements OnInit {
     this.prepareChart()
   }
 
-  private data2color(attempted: boolean, correct: boolean, partial: boolean, guess: boolean): string {
+  private resultObj(qid: number): ResultObj {
+    let defbool = (a: boolean, b: boolean) => Lib.isNil(a) ? b : a
+    let defnum = (a: number, b: number) => Lib.isNil(a) ? b : a
+    let isOmitted = this.result.isOmitted(qid)
+    let isAttempted = defbool(this.result.isAttempted(qid), false)
+    let isCorrect = defbool(this.result.isCorrect(qid), false)
+    let isPartial = defbool(this.result.isPartial(qid), false)
+    let isGuessing = defbool(this.result.guessings[qid], false)
+    let marks = this.result.marks(qid)
+    let guess = (!isOmitted && isAttempted && isGuessing) ? marks.value : 0
+    let sure = (!isOmitted && isAttempted && !isGuessing) ? marks.value : 0
+    let duration = defnum(this.result.durations[qid], 0)
+    let omitted = (isOmitted) ? marks.max : 0
+    let skipped = (!isOmitted && !isAttempted) ? marks.max : 0
+    let scored = sure + guess
+    return {
+      isOmitted: isOmitted, isAttempted: isAttempted, isCorrect: isCorrect,
+      isPartial: isPartial, isGuessing: isGuessing,
+      duration: duration,
+      max: marks.max, omitted: omitted, skipped: skipped,
+      guess: guess, sure: sure, scored: scored
+    }
+  }
+
+  private data2color(ro: ResultObj): string {
     let color = '49,176,213,1' // blue
-    if (attempted) {
-      if (correct) color = '0,128,0'
-      else if (partial) color = '255,165,0'
+    if (ro.isAttempted) {
+      if (ro.isCorrect) color = '0,128,0'
+      else if (ro.isPartial) color = '255,165,0'
       else color = '255,0,0'
-      if (guess) color += ',0.7'
+      if (ro.isGuessing) color += ',0.7'
       else color += ',1'
     }
     return color
   }
 
   private prepareChart() {
-    let defbool = (a: boolean, b: boolean) => Lib.isNil(a) ? b : a
-    let defnum = (a: number, b: number) => Lib.isNil(a) ? b : a
     this.chartArray = []
-    this.result.questions.forEach((q, qid) => {
-      let marksobj = this.result.marks(qid)
-      let value = defnum(this.result.durations[qid], 0)
-      let attempted = defbool(this.result.isAttempted(qid), false)
-      let correct = defbool(this.result.isCorrect(qid), false)
-      let partial = defbool(this.result.isPartial(qid), false)
-      let guess = defbool(this.result.guessings[qid], false)
-      let marks = defnum(marksobj.value, 0)
-      let max = defnum(marksobj.max, 1)
-      let color = this.data2color(attempted, correct, partial, guess)
-      let flags = (n: number) => [
-        marks + '/' + max,
-        Lib.timize(value),
-        (n + 1),
-      ]
-      let action = () => {
-        console.log('action', qid)
-        this.router.navigate(['/question', this.result.id, qid])
-      }
-      this.chartArray.push({ value: value, color: color, flags: flags, action: action })
+    this.result.questions.forEach((_, qid) => {
+      let ro = this.resultObj(qid)
+      let score = ro.scored + '/' + ro.max
+      this.chartArray.push({
+        value: ro.duration,
+        color: this.data2color(ro),
+        flags: (n: number) => ["" + score, Lib.timize(ro.duration), "" + (n + 1)],
+        action: () => this.router.navigate(['/question', this.result.id, qid])
+      })
     })
-  }
-
-  assess(omitted: boolean, attempted: boolean, guessed: boolean, marks: Marks): any[] {
-    let obj = { Sure: 0, Guess: 0, Marks: 0, Total: 0, Skipped: 0, Omitted: 0 }
-    obj.Total = marks.max
-    if (omitted) obj.Omitted = marks.max
-    else if (!attempted) obj.Skipped = marks.max
-    else if (guessed) obj.Guess = marks.value
-    else obj.Sure = marks.value
-    obj.Marks = obj.Sure + obj.Guess
-    return [obj.Sure, obj.Guess, obj.Marks, "percent", obj.Total, obj.Skipped, obj.Omitted]
   }
 
   get data() {
@@ -85,23 +102,20 @@ export class ResultComponent implements OnInit {
       }
     }
 
-    let defval = (a: boolean, b: boolean) => Lib.isNil(a) ? b : a
-    let isNum = (o) => typeof (o) === "number"
+    let isNum = (o: any) => typeof (o) === "number"
     let addArrays = (d: any[], s: any[]) => d.forEach((v, i) => { if (isNum(v)) d[i] += s[i] })
+    let o2a = (o: ResultObj) => [
+      o.sure, o.guess, o.scored, "percent", o.max, o.skipped, o.isOmitted
+    ]
 
     this.result.questions.forEach((q, qid) => {
-      let omitted = this.result.isOmitted(qid)
-      let attempted = this.result.isAttempted(qid)
-      let guess = defval(this.result.guessings[qid], false)
-      let marks = this.result.marks(qid)
-      addArrays(out.totals, this.assess(omitted, attempted, guess, marks))
+      let ro = this.resultObj(qid)
+      addArrays(out.totals, o2a(ro))
 
-      out.leaves.push(...q.tags
-        .map(t => t.title.split(":"))
-        .map(p => ({
-          type: p[0].trim(), name: p[1].trim(),
-          values: this.assess(omitted, attempted, guess, marks)
-        })))
+      out.leaves.push(...q.tags.map(t => t.title.split(":")).map(p => ({
+        type: p[0].trim(), name: p[1].trim(),
+        values: o2a(ro)
+      })))
     })
     return out
   }
